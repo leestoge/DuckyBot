61c6b79afc5544fbc5756df4820390fe03cdff45
Lee <L918Sturrock@hotmail.com>
2018-09-28 17:30:44 +0100

    Fixed some formatting errors, tried to make !idiot error checking work properly
    !idiot error checking still doesn't work

--- DuckyBot/Core/Modules/Commands/MiscellaneousModule.cs
+++ DuckyBot/Core/Modules/Commands/MiscellaneousModule.cs
@@ -23,17 +23,15 @@ namespace DuckyBot.Core.Modules.Commands
             target = mentionedUser ?? Context.User;
             var account = UserAccounts.GetAccount(target); // get the users account info by passing in the obtained user ID
             await Context.Channel.SendMessageAsync($"{target.Mention} has {account.XP} XP!"); // reply mentioning the user and stating their current XP
-            Console.WriteLine($"{DateTime.Now.ToString("t")} !stats executed successfully in " + Context.Guild.Name);
         }
         [Command("SetGame")] // Command declaration
         [Summary("Sets a 'Game' for the bot :video_game: (Only Moderators can use this command)")] // command summary
-        [RequireUserPermission(GuildPermission.Administrator)] ///Needed User Permissions ///
-        public async Task setgame([Remainder] string game) // command async task that takes in a parameter (remainder represents a space between the command and the parameter)
+        [RequireUserPermission(GuildPermission.Administrator)] // Needed User Permissions //
+        public async Task Setgame([Remainder] string game) // command async task that takes in a parameter (remainder represents a space between the command and the parameter)
         {
-            await (Context.Client as DiscordSocketClient).SetGameAsync(game); // change bots playing status to the provided string parameter
+            await Context.Client.SetGameAsync(game); // change bots playing status to the provided string parameter
             await Context.Channel.SendMessageAsync($"Successfully set my playing status to '**{game}**'"); // notify user in the text channel the command was used in
             Console.WriteLine($"{DateTime.Now}: Game was changed to {game}"); // notify me in console log
-            Console.WriteLine($"{DateTime.Now.ToString("t")} !setgame executed successfully in " + Context.Guild.Name);
         }
         [Command("idiot")] // Command declaration
         [Summary("@idiot idiot :drooling_face:")] // command summary
@@ -42,39 +40,32 @@ namespace DuckyBot.Core.Modules.Commands
             var user = Context.User; // store the user that used the command as var user
             var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == "Idiot"); // store the server role called "Idiot" as var role
 
-            await (user as IGuildUser).AddRoleAsync(role); // modify var users role
+            await ((IGuildUser) user).AddRoleAsync(role); // modify var users role
             await Context.Channel.SendMessageAsync(Context.User.Mention + " idiot"); // notify user in the text channel the command was used in
-            Console.WriteLine($"{DateTime.Now.ToString("t")} !idiot executed successfully in " + Context.Guild.Name);
 
-            if (role.Name != "Idiot")
+            if (role is null) // If role not found
             {
-                await Context.Channel.SendMessageAsync("oh fuck"); // notify user in the text channel the command was used in
-                return;
+                await Context.Channel.SendMessageAsync("Cannot find role `Idiot`."); // notify user
             }
         }
         [Command("dm")] // Command declaration
         [Summary("Directly message the bot owner. :writing_hand:")] // command summary
-        public async Task sendmsgtoowner([Remainder] string text) // command async task that takes in a parameter (remainder represents a space between the command and the parameter)
+        public async Task Sendmsgtoowner([Remainder] string text) // command async task that takes in a parameter (remainder represents a space between the command and the parameter)
         {
             await Context.Channel.SendMessageAsync(Context.User.Mention + " your message has been sent!"); // notify user in the text channel the command was used in
 
-            var embed = new EmbedBuilder() // create new embedded message
-            {
-                Color = new Color(255, 82, 41) // set colour of embedded message trim (to orange)
-            };
             var application = await Context.Client.GetApplicationInfoAsync(); // gets channels from discord client
             var z = await application.Owner.GetOrCreateDMChannelAsync(); // find my dm channel in order to private message me
             //embed.Description = $"`{Context.User.Username}` **from** `{Context.Guild.Name}` **sent you a message!**\n\n{text}"; // set embedded message description as user sent a message (With their input message)
             //await z.SendMessageAsync("", false, embed); // private message me with the users message
             await z.SendMessageAsync($"`{Context.User.Username}` **from** `{Context.Guild.Name}` **sent you a message!**\n\n{text}");
-            Console.WriteLine($"{DateTime.Now.ToString("t")} !dm executed successfully in " + Context.Guild.Name);
         }
         [Command("vid")] // Command declaration
         [Alias("Vid", "video", "vids")] // command aliases (also trigger task)
         [Summary("Posts a random Ducky related video :popcorn:")] // command summary 
         public async Task DuckyVideo() // command async task (method basically)
         {
-            string[] vids = new string[] // array of strings
+            var vids = new[] // array of strings
             {
                 "https://youtu.be/w1hZajJykW4",
                 "https://youtu.be/JxVunljhq7s",
@@ -87,10 +78,9 @@ namespace DuckyBot.Core.Modules.Commands
                 "https://clips.twitch.tv/ShyGleamingWormUnSane",
                 "https://clips.twitch.tv/KitschyElegantLocustBIRB",
             }; // array of strings called "vids" that holds the video links
-            int rand = StaticRandom.Instance.Next(vids.Length); // get random number between 0 and array length
-            string vidtopost = vids[rand]; // store string at the random number position in the array
+            var rand = StaticRandom.Instance.Next(vids.Length); // get random number between 0 and array length
+            var vidtopost = vids[rand]; // store string at the random number position in the array
             await ReplyAsync(vidtopost); // send file at our string file path we randomly get
-            Console.WriteLine($"{DateTime.Now.ToString("t")} !vid executed successfully in " + Context.Guild.Name);
         }
         [Command("flip")]
         [Summary("Flips a coin :moneybag:")]
@@ -99,17 +89,17 @@ namespace DuckyBot.Core.Modules.Commands
         {
             string flipToSend;
             int flip = StaticRandom.Instance.Next(1, 3);
-            if ((flip == 1) && !(flip == 2))
+            if ((flip == 1) && flip != 2)
             {
                 flipToSend = "Heads";
             }
-            else if ((flip == 2) && !(flip == 1))
+            else if ((flip == 2) && flip != 1)
             {
                 flipToSend = "Tails";
             }
             else
             {
-                await Context.Channel.SendMessageAsync("Something has went wrong! Stoge has been notified."); // tell user an error occured
+                await Context.Channel.SendMessageAsync("Something has went wrong! STOGE has been notified."); // tell user an error occurred
 
                 var application = await Context.Client.GetApplicationInfoAsync(); // gets channels from discord client
                 var z = await application.Owner.GetOrCreateDMChannelAsync(); // find my dm channel in order to private message me
@@ -119,7 +109,6 @@ namespace DuckyBot.Core.Modules.Commands
                 return;
             }
             await Context.Channel.SendMessageAsync(flipToSend);
-            Console.WriteLine($"{DateTime.Now.ToString("t")} !flip executed successfully in " + Context.Guild.Name);
         }
     }
 }
