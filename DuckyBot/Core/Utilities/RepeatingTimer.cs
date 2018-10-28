4e92af9d3ea95092ff037b240e659ea204a17dd7
Lee <L918Sturrock@hotmail.com>
2018-10-28 02:28:36 +0000

    Added status switcher
    Will switch bots "Playing ___" status every half hour.

--- DuckyBot/Core/Utilities/RepeatingTimer.cs
+++ DuckyBot/Core/Utilities/RepeatingTimer.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using System.Timers;
 using DuckyBot.Core.Main;
+using static DuckyBot.Core.Utilities.RandomGen;
 
 namespace DuckyBot.Core.Utilities
 {
@@ -11,22 +12,35 @@ namespace DuckyBot.Core.Utilities
         {
             var loopingTimer = new Timer
             {
-                Interval = 1800000, // every half hour 1800000
+                Interval = 1800000, // every half hour
                 AutoReset = true,
                 Enabled = true
             };
 
-            loopingTimer.Elapsed += OnTimerTicked;
+             loopingTimer.Elapsed += OnTimerTickedAsync;
             return Task.CompletedTask;
         }
 
-        private static void OnTimerTicked(object sender, ElapsedEventArgs e)
+        private static async void OnTimerTickedAsync(object sender, ElapsedEventArgs e)
         {
+            var predictionsTexts = new[] // array of strings called "predictionsTexts"
+            {
+                "flingmeoff mingoff",
+                "with CPU cooling liquid",
+                "DuckyBot | !help",
+                "with a long john",
+                "with a fast pounding long john",
+                "with eggs",
+                "with a long john teleporter",
+            };
+            var rand = Instance.Next(predictionsTexts.Length); // get random number between 0 and array length
+            var text = predictionsTexts[rand]; // store string at the random number position in the array
             if (Global.Client == null)
             {
                 Console.WriteLine("Timer ticked before the client was ready."); // error checking
             }
-            // post message
+
+            if (Global.Client != null) await Global.Client.SetGameAsync(text);
         }
     }
 }
