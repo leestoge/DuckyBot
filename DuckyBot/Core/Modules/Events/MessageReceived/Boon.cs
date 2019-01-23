4fb042d791291fc39a95fb2aa70f8b92a6b7b8db
Lee <L918Sturrock@hotmail.com>
2019-01-23 17:10:38 +0000

    Added functionality to react with multiple of the same amount (as BOTs techincally have Discord Nitro)

--- DuckyBot/Core/Modules/Events/MessageReceived/Boon.cs
+++ DuckyBot/Core/Modules/Events/MessageReceived/Boon.cs
@@ -12,11 +12,14 @@ namespace DuckyBot.Core.Modules.Events.MessageReceived
             if (msg.Author.Id == 200026014032592897) // if boon types
             {
                 var usermsg = msg as IUserMessage;
-                var emote = Emote.Parse("<:Pepega:507969601556971540>"); // give him pepega reaction
-                await Task.Delay(1500).ConfigureAwait(false);
+                var Pepega1 = Emote.Parse("<:Pepega:504690810143375372>"); // give him pepega reaction
+                var Pepega2 = Emote.Parse("<:Pepega:507969601556971540>"); // give him pepega reaction
+                await Task.Delay(2000).ConfigureAwait(false);
                 if (usermsg != null)
                 {
-                    await usermsg.AddReactionAsync(emote);
+                    await usermsg.AddReactionAsync(Pepega1);
+                    await Task.Delay(1500).ConfigureAwait(false);
+                    await usermsg.AddReactionAsync(Pepega2);
                 }
             }
         }
