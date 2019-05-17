fc4c192865d9656ab17e9823147b8541a9de1684
Lee <L918Sturrock@hotmail.com>
2019-05-17 22:20:08 +0100

    Added more people to User IDs

--- DuckyBot/Core/Utilities/UserIDs.cs
+++ DuckyBot/Core/Utilities/UserIDs.cs
@@ -5,7 +5,8 @@ namespace DuckyBot.Core.Utilities
     {
         internal static ulong Me = 98551113795530752;
         internal static ulong Ducky = 98543769795723264;
-        internal static ulong Boon = 193075985267163136;
+        internal static ulong Boon = 200026014032592897;
         internal static ulong trucks = 98511845438009344;
+        internal static ulong Whiskers = 193075985267163136;
     }
 }
