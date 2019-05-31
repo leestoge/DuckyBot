582305288a7157cd0a5e2b63b1b9320e0c11ebe6
Lee <L918Sturrock@hotmail.com>
2019-05-31 03:25:10 +0100

    Removed self from user IDs as my ID can be derived from application.Owner etc

--- DuckyBot/Core/Utilities/UserIDs.cs
+++ DuckyBot/Core/Utilities/UserIDs.cs
@@ -3,7 +3,6 @@ namespace DuckyBot.Core.Utilities
 {
     internal static class UserIDs
     {
-        internal static ulong Me = 98551113795530752;
         internal static ulong Ducky = 98543769795723264;
         internal static ulong Boon = 200026014032592897;
         internal static ulong trucks = 98511845438009344;
