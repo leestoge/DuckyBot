5a297b8c447dd38ceb720171fc97e4c501c256a4
Lee <L918Sturrock@hotmail.com>
2019-07-29 01:12:27 +0100

    Removed rarely used global getter/setter

--- DuckyBot/Core/Main/Global.cs
+++ DuckyBot/Core/Main/Global.cs
@@ -5,6 +5,5 @@ namespace DuckyBot.Core.Main
     internal static class Global
     {
         internal static DiscordSocketClient Client { get; set; }
-        internal static ulong MessageIdToTrack { get; set; }
     }
 }
