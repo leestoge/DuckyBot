a1e590cb1260afcdeff304bee2eb81503b1d2e31
Lee <L918Sturrock@hotmail.com>
2019-08-07 03:18:12 +0100

    Increased delay on message deletion logging to 5 seconds

--- DuckyBot/Core/Main/DuckyBotClient.cs
+++ DuckyBot/Core/Main/DuckyBotClient.cs
@@ -170,7 +170,7 @@ namespace DuckyBot.Core.Main
                 return;
             }
 
-            await Task.Delay(3000).ConfigureAwait(false);
+            await Task.Delay(5000).ConfigureAwait(false);
             await channel.SendMessageAsync($"**[{DateTime.Now}]** **[DELETED MESSAGE]** {msg.Author.Username}: {msg.Content}");
         }
 
