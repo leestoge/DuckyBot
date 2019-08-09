c01b289b4697737ccee52c691ecb2309d1807113
Lee <L918Sturrock@hotmail.com>
2019-08-09 12:35:50 +0100

    Added if statement to verify the user in context is a TDP Moderator, simplified message deletion command function

--- DuckyBot/Core/Modules/Commands/ModerationModule.cs
+++ DuckyBot/Core/Modules/Commands/ModerationModule.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Discord.Commands;
 using System.Threading.Tasks;
 using Discord;
@@ -12,21 +13,27 @@ namespace DuckyBot.Core.Modules.Commands
         [Command("Delete")] // Command declaration
         [Alias("Cleanup")] // command aliases (also trigger task)
         [Summary("Deletes DuckyBot's most recent messages to prevent chat flood. Can only delete between 1-100 Messages at a time. :shower: (Only Moderators can use this command)")] // command summary
-        [RequireUserPermission(GuildPermission.Administrator)] // Needed User Permissions //
         [RequireBotPermission(GuildPermission.ManageMessages)] //Needed Bot Permissions - must be set by the server administrator
-        public async Task Purge(int num) // command async task that takes in a parameter (remainder represents a space between the command and the parameter)
+        public async Task Delete(int num) // command async task that takes in a parameter (remainder represents a space between the command and the parameter)
         {
-            var messages = await Context.Channel.GetMessagesAsync(num + 1).FlattenAsync();
-            await ((ITextChannel) Context.Channel).DeleteMessagesAsync(messages);
+            if (((SocketGuildUser)Context.User).Roles.Any(r => r.Name == "Mod"))
+            {
+                await ((SocketTextChannel) Context.Message.Channel).DeleteMessagesAsync(await Context.Message.Channel.GetMessagesAsync(num + 1).FlattenAsync());
 
-            var notify = await Context.Channel.SendMessageAsync($"Successfully deleted `{num}` messages."); // notify user of message deletion success
-            await Task.Delay(3000).ConfigureAwait(false);
-            await ((ITextChannel) Context.Channel).DeleteMessageAsync(notify);
-
+                var notify = await ReplyAsync($"Successfully deleted `{num}` messages."); // notify user of message deletion success
+                await Task.Delay(3000).ConfigureAwait(false);
+                await ((SocketTextChannel)Context.Message.Channel).DeleteMessageAsync(notify);
+            }
+            else
+            {
+                var notify = await ReplyAsync("You are not a TDP Moderator and therefore cannot use this command!"); // notify user of message deletion success
+                await Task.Delay(3000).ConfigureAwait(false);
+                await ((ITextChannel)Context.Channel).DeleteMessageAsync(notify);
+            }         
         }
+
         [Command("Ban")] // Command declaration
         [Summary("Ban @Username :octagonal_sign: (Only Moderators can use this command)")] // command summary
-        [RequireUserPermission(GuildPermission.Administrator)] // Needed User Permissions //
         [RequireBotPermission(GuildPermission.BanMembers)] //Needed Bot Permissions - must be set by the server administrator
         public async Task BanAsync(SocketGuildUser user = null, [Remainder] string reason = null) // command async task that takes in a parameter (remainder represents a space between the command and the parameter)
         // parameters take in a mentioned user (which defaults to null), then a reason for the banning process
@@ -41,16 +48,25 @@ namespace DuckyBot.Core.Modules.Commands
                 throw new ArgumentException("You must provide a reason!"); // if no reason stated, notify command user that they must provide a ban reason
             }
 
-            var gld = Context.Guild as SocketGuild; // store server in context (aka guild) as var gld
-            var embed = new EmbedBuilder(); // create new embeded message //
-            embed.WithColor(new Color(255, 82, 41)); // set embedded message colour to orange
-            embed.ThumbnailUrl = user.GetAvatarUrl(); // set embedded message thumbnail to the mentioned users avatar
-            embed.Title = $"**{user.Username}** was banned from {user.Guild.Name}!"; // set embedded message title to who was banned
-            embed.Description = $"**Username: **{user.Username}\n**Banned by: **{Context.User.Mention}!\n**Reason: **{reason}"; // set embedded description to who the mentioned user was banned by
+            if (((SocketGuildUser)Context.User).Roles.Any(r => r.Name == "Mod"))
+            {
+                var gld = Context.Guild as SocketGuild; // store server in context (aka guild) as var gld
+                var embed = new EmbedBuilder(); // create new embeded message //
+                embed.WithColor(new Color(255, 82, 41)); // set embedded message colour to orange
+                embed.ThumbnailUrl = user.GetAvatarUrl(); // set embedded message thumbnail to the mentioned users avatar
+                embed.Title = $"**{user.Username}** was banned from {user.Guild.Name}!"; // set embedded message title to who was banned
+                embed.Description = $"**Username: **{user.Username}\n**Banned by: **{Context.User.Mention}!\n**Reason: **{reason}"; // set embedded description to who the mentioned user was banned by
 
-            await gld.AddBanAsync(user); // adds mentioned user to the var gld banned list (Server in context) - different ban lists per server
-            var final = embed.Build();
-            await Context.Channel.SendMessageAsync("", false, final); // send the embedded message back to the command using user
+                await gld.AddBanAsync(user); // adds mentioned user to the var gld banned list (Server in context) - different ban lists per server
+                var final = embed.Build();
+                await Context.Channel.SendMessageAsync("", false, final); // send the embedded message back to the command using user
+            }
+            else
+            {
+                var notify = await ReplyAsync("You are not a TDP Moderator and therefore cannot use this command!"); // notify user of message deletion success
+                await Task.Delay(3000).ConfigureAwait(false);
+                await ((ITextChannel)Context.Channel).DeleteMessageAsync(notify);
+            }
         }
         [Command("Remove")] // Command declaration
         [Summary("Remove @Username from the server :door: :point_left: (Only Moderators can use this command)")] // command summary
@@ -69,15 +85,24 @@ namespace DuckyBot.Core.Modules.Commands
                 throw new ArgumentException("You must provide a reason!"); // if no reason stated, notify command user that they must provide a remove reason
             }
 
-            var embed = new EmbedBuilder(); // create new embedded message //
-            embed.WithColor(new Color(255, 82, 41)); // set embedded message colour to orange
-            embed.ThumbnailUrl = user.GetAvatarUrl(); // set embedded message thumbnail to the mentioned users avatar
-            embed.Title = $" {user.Username} has been removed from {user.Guild.Name}!"; // set embedded message title to who was removed
-            embed.Description = $"**Username: **`{user.Username}`\n**Removed by: **{Context.User.Mention}!\n**Reason: **`{reason}`"; // set embedded description to who the mentioned user was removed by
+            if (((SocketGuildUser)Context.User).Roles.Any(r => r.Name == "Mod"))
+            {
+                var embed = new EmbedBuilder(); // create new embedded message //
+                embed.WithColor(new Color(255, 82, 41)); // set embedded message colour to orange
+                embed.ThumbnailUrl = user.GetAvatarUrl(); // set embedded message thumbnail to the mentioned users avatar
+                embed.Title = $" {user.Username} has been removed from {user.Guild.Name}!"; // set embedded message title to who was removed
+                embed.Description = $"**Username: **`{user.Username}`\n**Removed by: **{Context.User.Mention}!\n**Reason: **`{reason}`"; // set embedded description to who the mentioned user was removed by
 
-            await user.KickAsync(); // removes mentioned user
-            var final = embed.Build();
-            await Context.Channel.SendMessageAsync("", false, final); // send the embedded message back to the command using user
+                await user.KickAsync(); // removes mentioned user
+                var final = embed.Build();
+                await Context.Channel.SendMessageAsync("", false, final); // send the embedded message back to the command using user
+            }
+            else
+            {
+                var notify = await ReplyAsync("You are not a TDP Moderator and therefore cannot use this command!"); // notify user of message deletion success
+                await Task.Delay(3000).ConfigureAwait(false);
+                await ((ITextChannel)Context.Channel).DeleteMessageAsync(notify);
+            }           
         }
     }
 }
