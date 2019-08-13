f5696599deaa7ea7c479764b60ac48ee07709d24
Lee <L918Sturrock@hotmail.com>
2019-08-13 04:54:18 +0100

    Changed all current user IDs to readonly as they would never be rewritten

--- DuckyBot/Core/Utilities/UserIDs.cs
+++ DuckyBot/Core/Utilities/UserIDs.cs
@@ -2,9 +2,9 @@
 {
     internal static class UserIDs
     {
-        internal static ulong Ducky = 98543769795723264;
-        internal static ulong Boon = 200026014032592897;
-        internal static ulong trucks = 98511845438009344;
-        internal static ulong Whiskers = 193075985267163136;
+        internal static readonly ulong Ducky = 98543769795723264;
+        internal static readonly ulong Boon = 200026014032592897;
+        internal static readonly ulong trucks = 98511845438009344;
+        internal static readonly ulong Whiskers = 193075985267163136;
     }
 }
